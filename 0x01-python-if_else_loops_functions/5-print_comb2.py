dba311f5747680709ee39b0b93576f9065f7a8b9
StellaMontso <marystella.montso@gmail.com>
2022-08-23 13:20:56 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    f num != 99:
+    if num != 99:
         print("{:02d}, ".format(num), end='')
-        else:
-            print("{:02d}".format(num))
+    else:
+        print("{:02d}".format(num))
